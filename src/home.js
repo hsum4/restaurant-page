71a51353ff4570c333c9d91ef5a4b555da68e6cf
hsum4 <lancelav1@gmail.com>
2025-12-16 16:44:45 +0800

    finish section 3 and adjust spacing in template.html

--- src/home.js
+++ src/home.js
@@ -3,24 +3,23 @@ import junoImage from "./juno.jpg"
 export function loadHome() {
     const content = document.getElementById('content');
 
-    //////////////////////////////////////////////
     //Section 1 'title'
-    const header = document.createElement("div");
-    header.className = "section box";
+    const sectionOne = document.createElement("div");
+    sectionOne.className = "section box";
 
-    const headerTextElement = document.createElement("h1");
-    headerTextElement.textContent = "Juno's Filipino Cooking";
+    const headerText = document.createElement("h1");
+    headerText.textContent = "Juno's Filipino Cooking";
 
-    header.appendChild(headerTextElement);
-    content.appendChild(header);
-    /////////////////////////////////////////////
+    sectionOne.appendChild(headerText);
+    content.appendChild(sectionOne);
+
     //Section 2 'description'
 
-    const description = document.createElement("div");
-    description.className = "section box";
+    const sectionTwo = document.createElement("div");
+    sectionTwo.className = "section box";
 
-    const descriptionSpanElement = document.createElement("span");
-    descriptionSpanElement.textContent = `Juno's is the kind of place that makes you feel right at home. 
+    const descriptionSpan = document.createElement("span");
+    descriptionSpan.textContent = `Juno's is the kind of place that makes you feel right at home. 
                 The warm, inviting atmosphere and the friendly service will have you feeling like you're part of the family. 
                 Every bite of their delicious Filipino dishes feels like a comforting hug, 
                 just like the love you get from Juno himself. It's the kind of spot you'll 
@@ -32,7 +31,35 @@ export function loadHome() {
     junoImg.id = "junoimg";
     junoImg.alt = "a picture of Junoboy the Shih-Tzu!"
 
-    description.appendChild(descriptionSpanElement);
-    description.appendChild(junoImg);
-    content.appendChild(description);
+    sectionTwo.appendChild(descriptionSpan);
+    sectionTwo.appendChild(junoImg);
+    content.appendChild(sectionTwo);
+
+    //Section 3
+
+    const sectionThree = document.createElement("div");
+    sectionThree.className = "section box";
+
+    const hoursHeader = document.createElement("h2");
+    hoursHeader.textContent = "Hours";
+
+    sectionThree.appendChild(hoursHeader);
+
+    const hours = {
+        "Sunday": "8am - 8pm",
+        "Monday": "6am - 6pm",
+        "Tuesday": "6am - 6pm",
+        "Wednesday": "6am - 6pm",
+        "Thursday": "6am - 6pm",
+        "Friday": "6am - 6pm",
+        "Sunday": "8am - 8pm"
+    }
+
+    Object.entries(hours).forEach(([key, value]) => {
+        const span = document.createElement("span");
+        span.textContent = `${key}: ${value}`;
+        sectionThree.appendChild(span);
+    });
+    
+    content.appendChild(sectionThree);
 }
